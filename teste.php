418f924a4e984a58901168cc7ed83a4602d6df81
chiclete1802 <joaopaulokuchiki@gmail.com>
2025-09-01 08:49:43 -0300

    feat: teste conexao

---
+++ teste.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1";
+$username   = "root";
+$password   = "root";
+$dbname     = "aws_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+} else {
+    echo "Conectado com sucesso!";
+}
+?>
